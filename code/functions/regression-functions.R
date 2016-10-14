456d1f9e05477119d0c418864629eab18003aa78
Joseph Simonian <j.simonian10@gmail.com>
2016-10-14 16:09:02 -0700

    Add functions

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,31 @@
+
+# Residual Sum of Squares (RSS): given an "lm" object, compute the RSS
+residual_sum_squares <- function(model) {
+  sum(model$residuals^2)
+}
+
+# Total Sum of Squares (TSS): given an "lm" object, compute the TSS
+total_sum_squares <- function(model) {
+  sum(reg$effects^2) - reg$effects[[1]]^2
+}
+
+# R Squared: given an "lm" object, compute the R^2 value
+r_squared <- function(model) {
+  RSS <- residual_sum_squares(model)
+  TSS <- total_sum_squares(model)
+  1 - RSS/TSS
+}
+
+# F-Statistic: given an "lm" object, compute the F-statistic
+f_statistic <- function(model) {
+  p <- length(reg$coefficients) - 1
+  n <- length(reg$residuals)
+  RSS <- residual_sum_squares(model)
+  TSS <- total_sum_squares(model)
+  ((TSS - RSS) / p) / (RSS / (n - p - 1))
+}
+
+# Residual Standard Error (RSE): given an "lm" object, compute RSE
+residual_std_error <- function(model) {
+  sqrt(deviance(model)/df.residual(model))
+}
